d95929b4b04ec1ef921d8152ed312d05493f6158
Eric Nantes <ericnantes18@gmail.com>
2023-08-24 21:29:36 -0400

    Adding Toast for validation errors and adjusting create Post style

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,22 +7,31 @@ import { type RouterOutputs, api } from "~/utils/api";
 import dayjs from "dayjs";
 
 import relativeTime from "dayjs/plugin/relativeTime";
-import { LoadingPage } from "~/components/Loading";
+import { LoadingPage, LoadingSpinner } from "~/components/Loading";
 import { useState } from "react";
+import toast from "react-hot-toast";
 
 dayjs.extend(relativeTime);
 
 const CreatePostWizard = () => {
   const { user } = useUser();
 
-  const [type, setType] = useState("");
+  const [input, setInput] = useState("");
 
   const ctx = api.useContext();
 
   const { mutate, isLoading: isPosting } = api.post.create.useMutation({
     onSuccess: () => {
-      setType("");
+      setInput("");
       void ctx.post.getAll.invalidate();
+    },
+    onError: (error) => {
+      const validationError = error.data?.zodError?.fieldErrors.content;
+      if (validationError?.[0]) {
+        toast.error(validationError[0]);
+      } else {
+        toast.error("Failed to post, try again later.");
+      }
     },
   });
 
@@ -41,13 +50,29 @@ const CreatePostWizard = () => {
         type="text"
         placeholder="Type what you want!!"
         className="grow bg-transparent outline-none"
-        value={type}
-        onChange={(e) => setType(e.target.value)}
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
         disabled={isPosting}
+        onKeyDown={(e) => {
+          if (e.key === "Enter") {
+            e.preventDefault();
+            if (input !== "") {
+              mutate({ content: input });
+            }
+          }
+        }}
       />
-      <button type="submit" onClick={() => mutate({ content: type })}>
-        Post
-      </button>
+      {!isPosting && input !== "" && (
+        <button type="submit" onClick={() => mutate({ content: input })}>
+          Post
+        </button>
+      )}
+
+      {isPosting && (
+        <div className="relative flex items-center justify-center">
+          <LoadingSpinner size={20} />
+        </div>
+      )}
     </div>
   );
 };
