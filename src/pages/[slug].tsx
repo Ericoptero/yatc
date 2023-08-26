afb0fd06e89931987080cb19df6ae4d038f324dc
Eric Nantes <ericnantes18@gmail.com>
2023-08-25 22:02:45 -0400

    Creating Profile Router and configuring Server Side Helpers from React query

--- src/pages/[slug].tsx
+++ src/pages/[slug].tsx
@@ -1,6 +1,16 @@
+import type { GetStaticPropsContext, InferGetStaticPropsType } from "next";
 import Head from "next/head";
+import { api } from "~/utils/api";
 
-export default function ProfilePage() {
+type ProfilePageProps = InferGetStaticPropsType<typeof getStaticProps>;
+
+export default function ProfilePage({ username }: ProfilePageProps) {
+  const { data } = api.profile.getUserByUsername.useQuery({
+    username,
+  });
+
+  if (!data) return <div>Something went wrong</div>;
+
   return (
     <>
       <Head>
@@ -9,8 +19,42 @@ export default function ProfilePage() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className="flex h-screen justify-center">
-        <div>Profile Page</div>
+        <div>{data.username}</div>
       </main>
     </>
   );
 }
+
+import { createServerSideHelpers } from "@trpc/react-query/server";
+import superjson from "superjson";
+import { appRouter } from "~/server/api/root";
+import { prisma } from "~/server/db";
+
+type ProfileStaticPropsContext = GetStaticPropsContext<{ slug: string }>;
+
+export const getStaticProps = async (context: ProfileStaticPropsContext) => {
+  const helpers = createServerSideHelpers({
+    router: appRouter,
+    ctx: { prisma, userId: null },
+    transformer: superjson, // optional - adds superjson serialization
+  });
+
+  const slug = context.params?.slug;
+
+  if (typeof slug !== "string") return new Error("Invalid slug");
+
+  const username = slug.replace("@", "");
+
+  await helpers.profile.getUserByUsername.prefetch({ username: username });
+
+  return {
+    props: {
+      trpcState: helpers.dehydrate(),
+      username,
+    },
+  };
+};
+
+export const getStaticPaths = () => {
+  return { paths: [], fallback: "blocking" };
+};
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -83,31 +83,31 @@ const PostView = (props: PostWithUser) => {
   const { post, author } = props;
 
   return (
-    <Link href={`/post/${post.id}`}>
-      <div className="flex gap-3 border-b border-slate-400 p-4">
-        <Link href={`/@${author.username}`}>
-          <Image
-            src={author.imageUrl}
-            alt={`@${author.username}'s profile picture`}
-            className="h-14 w-14 rounded-full"
-            width={56}
-            height={56}
-          />
-        </Link>
-        <div className="flex flex-col">
-          <div className="flex gap-1 font-bold text-slate-300">
-            <Link href={`/@${author.username}`}>
-              <span>{`@${author.username}`}</span>
-            </Link>
+    <div className="flex gap-3 border-b border-slate-400 p-4">
+      <Link href={`/@${author.username}`}>
+        <Image
+          src={author.imageUrl}
+          alt={`@${author.username}'s profile picture`}
+          className="h-14 w-14 rounded-full"
+          width={56}
+          height={56}
+        />
+      </Link>
+      <div className="flex flex-col">
+        <div className="flex gap-1 font-bold text-slate-300">
+          <Link href={`/@${author.username}`}>
+            <span>{`@${author.username}`}</span>
+          </Link>
 
+          <Link href={`/post/${post.id}`}>
             <span className="font-thin">{`· ${dayjs(
               post.createdAt
             ).fromNow()}`}</span>
-          </div>
-          <span className="text-xl">{post.content}</span>
+          </Link>
         </div>
+        <span className="text-xl">{post.content}</span>
       </div>
-    </Link>
+    </div>
   );
 };
 
